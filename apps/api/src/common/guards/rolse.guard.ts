47686a55aafb72e1ceb940e532ddfd143ed573f4
Mahdi <mahdibagherichanel@gmail.com>
2026-05-02 23:56:36 +0330

    add gurd for auth

---
+++ apps/api/src/common/guards/rolse.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from '@shared/enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const checkRole = this.reflector.getAllAndOverride<string[]>(Roles, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!checkRole) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+    if (!user) {
+      return false;
+    }
+
+    return checkRole.includes(user.role);
+  }
+}
