cfdc44afc70f5642b4ea485774dd1f6c4b4836db
Mahdi <mahdibagherichanel@gmail.com>
2026-05-02 23:56:05 +0330

    set config for auth

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,43 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from '@shared/config/typeorm.config';
+import { JwtAuthGuard } from '@shared/guards/jwt-auth.guard';
+import { RolesGuard } from '@shared/guards/rolse.guard';
+import { AppCacheModule } from '@shared/modules/cache.module';
+import { CookieModule } from '@shared/modules/cookie.module';
+import { RedisModule } from '@shared/modules/redis.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_ACCESS_SECRET,
+      signOptions: {
+        expiresIn: Number(process.env.JWT_ACCESS_EXPIRY),
+      },
+    }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfig }),
+    RedisModule.forRootAsync(),
+    AppCacheModule,
     AuthModule,
     UserModule,
+    CookieModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule {}
