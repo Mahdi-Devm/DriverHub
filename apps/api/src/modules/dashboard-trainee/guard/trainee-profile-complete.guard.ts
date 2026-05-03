7bb9700c9c55f20caeef4849b5ef0a362dfdb8f2
Mahdi <mahdibagherichanel@gmail.com>
2026-05-03 21:30:54 +0330

    add guard for chech complet profile trainee or no

---
+++ apps/api/src/modules/dashboard-trainee/guard/trainee-profile-complete.guard.ts
@@ -0,0 +1,47 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DashboardTrainee } from '../entities/dashboard-trainee.entity';
+
+@Injectable()
+export class TraineeProfileCompleteGuard implements CanActivate {
+  constructor(
+    @InjectRepository(DashboardTrainee)
+    private traineeProfileRepository: Repository<DashboardTrainee>,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const skipCheck = this.reflector.getAllAndOverride<boolean>(
+      process.env.SKIP_TRAINEE_PROFILE_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (skipCheck) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user) {
+      throw new ForbiddenException('کاربر یافت نشد');
+    }
+
+    const profile = await this.traineeProfileRepository.findOne({
+      where: { user: { id: user.id } },
+    });
+
+    if (!profile || !profile.isProfileComplete) {
+      throw new ForbiddenException('لطفاً ابتدا پروفایل خود را تکمیل کنید');
+    }
+
+    return true;
+  }
+}
